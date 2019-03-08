f79949fc75aeea2190ba3a77bc722590b0281f14
szib <szib77@gmail.com>
2019-03-08 21:52:20 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,22 @@
-# Write your code here.
+def line(line)
+  if line.empty?
+    puts "The line is currently empty." 
+  else
+    customers = line.map.with_index { |name, idx| "#{idx+1}. #{name}"}
+    puts "The line is currently: #{customers.join(' ')}"
+  end
+end
+
+def take_a_number(line, name)
+  line << name
+  puts "Welcome, #{name}. You are number #{line.length} in line."
+end
+
+def now_serving(line)
+  if line.empty?
+    puts "There is nobody waiting to be served!" 
+  else
+    puts "Currently serving #{line.shift}."
+  end
+  line
+end
