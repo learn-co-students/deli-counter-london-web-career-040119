d088592615f67e2c72e945469f1e65c1e18af35e
Francisco Costa <francisco@fcosta.pt>
2019-03-15 11:37:45 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,14 @@
-# Write your code here.
+require 'pry'
+
+katz_deli = []
+
+def line(array)
+  current_line = []
+  if array.length == 0
+    puts "The line is currently empty."
+  else
+    array.each_with_index {|client, position| current_line << "#{position + 1}. #{client}"}
+    line_string = current_line.join(" ")
+    puts "The line is currently: #{line_string}"
+  end
+end
